0fddba5b7ce8322dbc7cece65fc4b891b17b97b1
agreenberg18 <ag98718n@pace.edu>
2016-06-15 19:06:24 -0400

    added static images for video tuts, and added example app1

--- appDock/app/pages/one/one.ts
+++ appDock/app/pages/one/one.ts
@@ -1,5 +1,13 @@
 import {Page, NavController} from 'ionic-angular';
-
+import {HomePage} from '../home/home';
+import {TutorialPage} from '../tutorial/tutorial';
+import {FeedbackPage} from '../feedback/feedback';
+import {SocialPage} from '../social/social';
+import {DevPage} from '../dev/dev';
+import {TwoPage} from '../two/two';
+import {ThreePage} from '../three/three';
+import {FourPage} from '../four/four';
+import {AboutPage} from '../about/about';
 /*
   Generated class for the OnePage page.
 
@@ -10,5 +18,14 @@ import {Page, NavController} from 'ionic-angular';
   templateUrl: 'build/pages/one/one.html',
 })
 export class OnePage {
+  homePage = HomePage;
+  tutorialPage = TutorialPage;
+  feedbackPage = FeedbackPage;
+  socialPage = SocialPage;
+  devPage = DevPage;
+  twoPage = TwoPage;
+  threePage = ThreePage;
+  fourPage = FourPage;
+  aboutPage = AboutPage;
   constructor(public nav: NavController) {}
 }
