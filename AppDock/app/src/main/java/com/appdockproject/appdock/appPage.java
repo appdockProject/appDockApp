9c5ff9bf18644f7e784e98f12c94657ead957ef6
Andrew Greenberg <andrew@Andrews-MacBook-Air.local>
2016-07-16 12:38:56 -0400

    android appdock

---
+++ AppDock/app/src/main/java/com/appdockproject/appdock/appPage.java
@@ -0,0 +1,13 @@
+package com.appdockproject.appdock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class appPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_page);
+    }
+}
