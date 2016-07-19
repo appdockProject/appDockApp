e65f06f6a16e336481230b8cd801649b2f70c368
Andrew Greenberg <andrew@Andrews-MacBook-Air.local>
2016-07-18 23:03:30 -0400

    Made all views full screen

--- NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/feedbackActivity.java
+++ NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/feedbackActivity.java
@@ -1,7 +1,9 @@
 package com.appdockproject.appdock;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class feedbackActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class feedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+            getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
+        }
     }
 }
