15e49cce30636d06a80e050363642e085f2cab8c
Andrew Greenberg <andrew@Andrews-MacBook-Air.local>
2016-07-18 22:47:05 -0400

    added dev page and nav

--- NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/appPage.java
+++ NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/appPage.java
@@ -1,14 +1,15 @@
 package com.appdockproject.appdock;
 
-<<<<<<< HEAD
+
+import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-=======
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
->>>>>>> a053b3727c9fd30e191b65d3a85fb16049cf9960
+import android.widget.Button;
+
 
 public class appPage extends AppCompatActivity {
 
@@ -16,12 +17,18 @@ public class appPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_page);
-<<<<<<< HEAD
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
             getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
         }
-=======
->>>>>>> a053b3727c9fd30e191b65d3a85fb16049cf9960
+    Button devBtn = (Button) findViewById(R.id.devBtn);
+
+        devBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(appPage.this, devActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
