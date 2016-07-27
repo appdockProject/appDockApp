6dd70b94001b2832cd0f51a8e587c03d76b958c8
Vanessa Rene <vjrene@gmail.com>
2016-07-27 18:37:55 -0400

    updated string resources

--- NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/dev1Activity.java
+++ NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/dev1Activity.java
@@ -3,5 +3,15 @@ package com.appdockproject.appdock;
 /**
  * Created by CS on 7/27/16.
  */
-public class dev1Activity {
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class dev1Activity extends AppCompatActivity {
+
+
 }
