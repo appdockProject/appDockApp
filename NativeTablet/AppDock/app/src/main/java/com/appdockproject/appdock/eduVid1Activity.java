bf26d13c3f988e8c449b59bd959f55b49c18c268
Vanessa Rene <vjrene@gmail.com>
2016-08-02 19:19:05 -0400

    added video

--- NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/eduVid1Activity.java
+++ NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/eduVid1Activity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 /**
@@ -14,36 +15,17 @@ import android.widget.VideoView;
  */
 public class eduVid1Activity extends AppCompatActivity {
 
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eduvid1);
 
-        Button playVideo = (Button) findViewById(R.id.button1);
+        String videoLink = "android.resource://com.vjrene.cs.videotest/"+R.raw.eduvideo1;
+        final VideoView videoView = (VideoView) findViewById(R.id.videoView);
 
-        getWindow().setFormat(PixelFormat.UNKNOWN);
+        videoView.setVideoPath(videoLink);
 
-        //displays video file
-        VideoView video1 = (VideoView) findViewById(R.id.videoView);
-
-        String uriPath = "android.resource://com.appdockproject.appdock/"+R.raw.crossdakarcity;
-        Uri uri = Uri.parse(uriPath);
-        video1.setVideoURI(uri);
-        video1.requestFocus();
-        video1.start();
-
-        playVideo.setOnClickListener(new Button.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                VideoView video1 = (VideoView) findViewById(R.id.videoView);
-
-                String uriPath = "android.resource://com.appdockproject.appdock/"+R.raw.crossdakarcity;
-                Uri uri = Uri.parse(uriPath);
-                video1.setVideoURI(uri);
-                video1.requestFocus();
-                video1.start();
-            }
-        });
+        videoView.start();
 
         }
     }
