ad832d2278a5f71bf809de3600eb114da88d60ed
Vanessa Rene <vjrene@gmail.com>
2016-10-05 17:12:37 -0400

    added listeners to buttons on video page

--- NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/eduVid1Activity.java
+++ NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/eduVid1Activity.java
@@ -1,5 +1,6 @@
 package com.appdockproject.appdock;
 
+import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Build;
@@ -24,6 +25,40 @@ public class eduVid1Activity extends AppCompatActivity {
             int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
             getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
         }
+
+        Button devBtn = (Button) findViewById(R.id.devBtn);
+        Button homeBtn = (Button) findViewById(R.id.appBtn);
+        Button cmntBtn = (Button) findViewById(R.id.comBtn);
+        Button fbBtn = (Button) findViewById(R.id.fbBtn);
+
+        devBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(eduVid1Activity.this, devActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        homeBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(eduVid1Activity.this, appPage.class);
+                startActivity(intent);
+            }
+        });
+
+        cmntBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(eduVid1Activity.this, feedbackActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        fbBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent intent = new Intent(eduVid1Activity.this, facebookActivity.class);
+                startActivity(intent);
+            }
+        });
 
         String videoLink = "android.resource://com.appdockproject.appdock/"+R.raw.eduvideo1;
         final VideoView videoView = (VideoView) findViewById(R.id.videoView);
