0d1d3d39ca35bb75545513fb87c4823ba7f70fae
Jan Gerhard Schøpp <jgschoepp@gmail.com>
2017-03-08 17:00:23 -0500

    Organized imports

--- NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/MainActivity.java
+++ NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends FragmentActivity {
 
             // Create a new Fragment to be placed in the activity layout
             appPage firstFragment = new appPage();
-            
+
             // In case this activity was started with special instructions from an
             // Intent, pass the Intent's extras to the fragment as arguments
             firstFragment.setArguments(getIntent().getExtras());
--- NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/eduActivity.java
+++ NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/eduActivity.java
@@ -1,21 +1,12 @@
 package com.appdockproject.appdock;
 
 import android.content.Intent;
-import android.os.Build;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
-
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-
-import static com.appdockproject.appdock.R.id.devBtn;
-import static com.appdockproject.appdock.R.id.fbBtn;
 
 public class eduActivity extends Fragment {
 
