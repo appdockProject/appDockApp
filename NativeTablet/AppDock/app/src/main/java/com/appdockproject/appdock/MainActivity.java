2119ec251cb92882939a208c376d25943f45c1b9
Jan Gerhard Schøpp <jgschoepp@gmail.com>
2017-03-08 16:49:17 -0500

    mend

--- NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/MainActivity.java
+++ NativeTablet/AppDock/app/src/main/java/com/appdockproject/appdock/MainActivity.java
@@ -87,8 +87,7 @@ public class MainActivity extends FragmentActivity {
 
             // Create a new Fragment to be placed in the activity layout
             appPage firstFragment = new appPage();
-            cmntBtn.setPressed(true);
-
+            
             // In case this activity was started with special instructions from an
             // Intent, pass the Intent's extras to the fragment as arguments
             firstFragment.setArguments(getIntent().getExtras());
